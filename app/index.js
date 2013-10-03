046d5355c8cc625ee1c0d0cc32f9f74a9db9a57b
Eric Clemmons <eric@smarterspam.com>
2013-10-03 11:09:50 -0500

    Prefer 192.168.x.x

--- app/index.js
+++ app/index.js
@@ -139,9 +139,9 @@ WordpressGenerator.prototype.promptForDatabase = function() {
 WordpressGenerator.prototype.promptForIp = function() {
   // Private IP blocks
   var blocks = [
-    ['10.0.0.0',    '10.255.255.255'],
+    ['192.168.0.0', '192.168.255.255'],
     ['172.16.0.0',  '172.31.255.255'],
-    ['192.168.0.0', '192.168.255.255']
+    ['10.0.0.0',    '10.255.255.255']
   ];
 
   // Long IP ranges
