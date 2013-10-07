a03de4644dc07502079883f33d63b997ad09f1d5
Eric Clemmons <eric@smarterspam.com>
2013-10-07 10:08:55 -0500

    Remove hard-coded deploy_to on staging

--- app/templates/deployment/stages/staging.rb
+++ app/templates/deployment/stages/staging.rb
@@ -2,7 +2,6 @@ role :db,           "staging.<%= props.domain %>"
 role :web,          "staging.<%= props.domain %>"
 
 set :stage,         "staging"
-set :deploy_to,     "/var/www/#{domain}/master"
 
 # Open site after deploying
 after "deploy" do
